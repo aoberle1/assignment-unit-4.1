8c4532b0188732763f8c2c8989a13f8dc77e92ea
Adam Oberle <oberle.adam@gmail.com>
2023-04-18 09:17:59 -0500

    finished 1 in assignment 4.1

--- assignment/scripts/function-practice.js
+++ assignment/scripts/function-practice.js
@@ -9,12 +9,13 @@ function hello() {
   return 'Hello World!';
 }
 // Call the function to test
-console.log('Test - should say "Hello World!"', hello());
+console.log( hello() );
 
 
 // 2. Function to return an personalized hello, using the `name` argument.
 //    for example 'Hello, Jo!', or 'Hello, Stacy!'
 function helloName( name ) {
+  
   return;
 }
 // Remember to call the function to test
